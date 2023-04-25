07640fa1ebd2cbb621919f55aa263c992a44b801
michelbdg <114735296+michelbdg@users.noreply.github.com>
2023-04-26 00:15:16 +0200

    modification(faire fichier audio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
+/* partie menu */
+
 let toggle = document.querySelector('.toggle');
 let body = document.querySelector('body');
 
 toggle.addEventListener('click', function(){
     body.classList.toggle('open')
 })
+
+/* Lecteur audio personnalisé */
+const audio = document.querySelector("audio");
+const track = document.querySelector("#track");
+const elapsed = document.querySelector("#elapsed");
+const trackTime = document.querySelector("#play-btn");
+const playButton = document.querySelector("#pause-btn");
+const pauseButton = document.querySelector("#stop-btn");
+const volume = document.querySelector("#volume");
+const volumeValue = document.querySelector("#volume-value");
+
+//On récupère la duréé du mp3
+
+let duration = audio.duration;
+
+console.log(duration)
